f87c0011d0fc57fdd81e9a1ff200f946ed12f053
Valeryfer <vafernandez@unal.edu.co>
2024-08-26 23:22:21 -0500

    Ya

--- Futbolista.py
+++ Futbolista.py
@@ -3,10 +3,10 @@ from persona import Persona
 
 class Futbolista(Persona, Deportista):
     lista_futbolistas = []
-
+    
     def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
         Persona.__init__(self, nombre, edad, altura, sexo)
-        Deportista.__init__(self, añosPracticando)
+        Deportista.__init__(self, "Futbol", añosPracticando)
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
